ba752b295821c989b9be93f9a238d079e6c87920
Sam Harwell <sam@tunnelvisionlabs.com>
2018-04-01 09:29:51 -0500

    Simplify package and command registration

--- GitExtensionsVSIX/GitExtensionsPackage.cs
+++ GitExtensionsVSIX/GitExtensionsPackage.cs
@@ -1,8 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
-
+﻿using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace GitExtensionsVSIX
 {
@@ -25,22 +22,16 @@ namespace GitExtensionsVSIX
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
-    [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
-    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
     [Guid(PackageGuids.guidGitExtensionsPackageString)]
-    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideBindingPath]
     public sealed class GitExtensionsPackage : Package
     {
-        #region Package Members
-
         protected override void Initialize()
         {
-            GitExtCommands.Initialize(this);
             base.Initialize();
-        }
 
-        #endregion
+            GitExtCommands.Initialize(this);
+        }
     }
 }
