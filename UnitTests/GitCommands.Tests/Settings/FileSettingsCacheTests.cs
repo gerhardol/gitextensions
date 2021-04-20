0f4b8beac5e85aa1d2b63d3e4984b51d841068d7
Gerhard Olsson <gerhardol@users.noreply.github.com>
2021-04-21 01:04:01 +0200

    C#9 new revert incorrect types

--- UnitTests/GitCommands.Tests/Settings/FileSettingsCacheTests.cs
+++ UnitTests/GitCommands.Tests/Settings/FileSettingsCacheTests.cs
@@ -50,7 +50,7 @@ namespace GitCommandsTests.Settings
         [TestCase("")]
         public void SaveImpl_should_throw_if_invalid_path(string settingsFilePath)
         {
-            MockFileSettingsCache cache = new(settingsFilePath, false).GetTestAccessor();
+            var cache = new MockFileSettingsCache(settingsFilePath, false).GetTestAccessor();
             cache.SetLastModificationDate(DateTime.Now);
             ((Action)(() => cache.SaveImpl())).Should().Throw<SaveSettingsException>();
         }
@@ -63,7 +63,7 @@ namespace GitCommandsTests.Settings
 
             try
             {
-                MockFileSettingsCache cache = new(settingsFilePath, false).GetTestAccessor();
+                var cache = new MockFileSettingsCache(settingsFilePath, false).GetTestAccessor();
                 cache.SetLastModificationDate(DateTime.Now);
 
                 Directory.Exists(tempPath).Should().BeFalse();
