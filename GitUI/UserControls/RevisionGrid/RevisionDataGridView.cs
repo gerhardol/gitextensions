gerhardol/gitextensions

commit e4522b70de6dab39503195e696bd66354f63341e

fixup! attempt improve test stability

GitUI/UserControls/RevisionGrid/RevisionDataGridView.cs
@@ -40,6 +40,10 @@ namespace GitUI.UserControls.RevisionGrid
 
         private readonly List<ColumnProvider> _columnProviders = new();
 
+        private IList<int> _toBeSelectedGraphIndexes = null;
+
+        private int _loadedToBeSelectedRevisionsCount = 0;
+
         private int _backgroundScrollTo;
         private int _consecutiveScrollMessageCnt = 0; // Is used to detect if a forced repaint is needed.
         private int _rowHeight; // Height of elements in the cache. Is equal to the control's row height.
@@ -50,10 +54,13 @@ namespace GitUI.UserControls.RevisionGrid
         private Font _boldFont;
         private Font _monospaceFont;
 
-        public bool UpdatingVisibleRows { get; private set; }
+        /// <summary>
+        ///  Indicates whether the data is currently being loaded, and whether it is safe to interact with the content of the grid,
+        ///  e.g., to read the selection.
+        /// </summary>
+        public bool IsDataLoadComplete { get; private set; } = true;
 
-        // Used in tests
-        public bool IsLoadingGrid { get; set; } = false;
+        public bool UpdatingVisibleRows { get; private set; }
 
         public RevisionDataGridView()
         {
@@ -168,10 +175,6 @@ namespace GitUI.UserControls.RevisionGrid
         // Contains the object Id's that will be selected as soon as all of them have been loaded.
         // The object Id's are in the order in which they were originally selected.
         public IReadOnlyList<ObjectId> ToBeSelectedObjectIds { get; set; } = Array.Empty<ObjectId>();
-        private IList<int> _toBeSelectedGraphIndexes = null;
-
-        private int _loadedToBeSelectedRevisionsCount = 0;
-
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         [Browsable(false)]
         public IReadOnlyList<ObjectId>? SelectedObjectIds
@@ -443,21 +446,35 @@ namespace GitUI.UserControls.RevisionGrid
                         {
                             EnsureRowVisible(GetGraphIndexes()[0]);
                         }
-                   }
+                    }
 
-                    IsLoadingGrid = false;
+                    MarkAsDataLoadingComplete();
                 })
                 .FileAndForget();
             }
             else
             {
-                IsLoadingGrid = false;
+                MarkAsDataLoadingComplete();
             }
 
             foreach (ColumnProvider columnProvider in _columnProviders)
             {
                 columnProvider.LoadingCompleted();
             }
+
+            return;
+
+            void MarkAsDataLoadingComplete()
+            {
+                Debug.Assert(IsDataLoadComplete, "The grid is already marked as 'data load complete'.");
+                IsDataLoadComplete = true;
+            }
+        }
+
+        public void MarkAsDataLoading()
+        {
+            Debug.Assert(!IsDataLoadComplete, "The grid is already marked as 'data load in process'.");
+            IsDataLoadComplete = false;
         }
 
         /// <summary>
@@ -854,8 +871,6 @@ namespace GitUI.UserControls.RevisionGrid
             {
                 _gridView = gridView;
             }
-
-            public bool IsGridUpdating => _gridView.IsLoadingGrid;
         }
     }
 }

GitUI/UserControls/RevisionGrid/RevisionGridControl.cs
@@ -914,7 +914,7 @@ namespace GitUI
                 _gridView.Enabled = true;
                 _gridView.Focus();
                 _gridView.SelectionChanged += OnGridViewSelectionChanged;
-                _gridView.IsLoadingGrid = true;
+                _gridView.MarkAsDataLoading();
 
                 // Add the spinner controls, removed by SetPage()
                 Controls.Add(_loadingControlSpinner);
@@ -2903,8 +2903,8 @@ namespace GitUI
 
             public int VisibleRevisionCount => _revisionGridControl._gridView.RowCount;
 
-            public bool IsUiStable =>
-                !_revisionGridControl._gridView.GetTestAccessor().IsGridUpdating;
+            public bool IsDataLoadComplete =>
+                !_revisionGridControl._gridView.IsDataLoadComplete;
 
             public void ClearSelection()
             {

IntegrationTests/UI.IntegrationTests/CommandsDialogs/FormBrowseTests.cs
@@ -306,7 +306,7 @@ namespace GitExtensions.UITests.CommandsDialogs
 
         private static void WaitForRevisionsToBeLoaded(FormBrowse form)
         {
-            UITest.ProcessUntil("Loading Revisions", () => form.GetTestAccessor().RevisionGrid.GetTestAccessor().IsUiStable);
+            UITest.ProcessUntil("Loading Revisions", () => form.GetTestAccessor().RevisionGrid.GetTestAccessor().IsDataLoadComplete);
         }
     }
 }

IntegrationTests/UI.IntegrationTests/CommandsDialogs/SplitterPersistenceTests.cs
@@ -134,7 +134,7 @@ namespace GitExtensions.UITests.CommandsDialogs
 
         private static void WaitForRevisionsToBeLoaded(RevisionGridControl revisionGridControl)
         {
-            UITest.ProcessUntil("Loading Revisions", () => revisionGridControl.GetTestAccessor().IsUiStable);
+            UITest.ProcessUntil("Loading Revisions", () => revisionGridControl.GetTestAccessor().IsDataLoadComplete);
         }
     }
 }

IntegrationTests/UI.IntegrationTests/UserControls/RevisionGrid/RevisionGridControlTests.cs
@@ -380,7 +380,7 @@ namespace GitExtensions.UITests.UserControls.RevisionGrid
 
         private static void WaitForRevisionsToBeLoaded(RevisionGridControl revisionGridControl)
         {
-            UITest.ProcessUntil("Loading Revisions", () => revisionGridControl.GetTestAccessor().IsUiStable);
+            UITest.ProcessUntil("Loading Revisions", () => revisionGridControl.GetTestAccessor().IsDataLoadComplete);
         }
     }
 }

commit 390407faf8d18a60c43d14f01f9075de31a068d5

fixup! Regression from e4522b70de6dab39503195e696bd66354f63341e
Do not scroll to rows if selection was changed

GitUI/UserControls/RevisionGrid/RevisionDataGridView.cs
@@ -127,11 +127,11 @@ namespace GitUI.UserControls.RevisionGrid
                     .FileAndForget();
             };
 
+            // Suppress CS8618, init in Clear().
+            _toBeSelectedGraphIndexesCache = new(() => CalculateGraphIndices());
+
             VirtualMode = true;
             Clear();
-
-            // Explicit init is required, calling ResetGraphIndices() is not enough
-            _toBeSelectedGraphIndexesCache = new(() => CalculateGraphIndices());
 
             return;
 
@@ -433,12 +433,22 @@ namespace GitUI.UserControls.RevisionGrid
                 {
                     await this.SwitchToMainThreadAsync();
 
-                    if (RowCount - 1 < ScrollTo())
+                    if (_toBeSelectedGraphIndexesCache.Value.Count == 0)
+                    {
+                        // Nothing to select or interrupted
+                        MarkAsDataLoadingComplete();
+                        return;
+                    }
+
+                    int scrollTo = _toBeSelectedGraphIndexesCache.Value.Max();
+                    int firstGraphIndex = _toBeSelectedGraphIndexesCache.Value[0];
+                    if (RowCount - 1 < scrollTo)
                     {
                         // Wait for the periodic background thread to load all rows in the grid
-                        while (RowCount - 1 < ScrollTo())
+                        while (_toBeSelectedGraphIndexesCache.IsValueCreated && RowCount - 1 < scrollTo)
                         {
-                            int maxScroll = Math.Min(RowCount - 1, ScrollTo());
+                            // Force loading of rows
+                            int maxScroll = Math.Min(RowCount - 1, scrollTo);
                             EnsureRowVisible(maxScroll);
 
                             // Wait for background thread to update grid rows
@@ -450,13 +460,12 @@ namespace GitUI.UserControls.RevisionGrid
                     {
                         // Rows already selected once, reselect and refresh
                         SelectRowsIfReady(RowCount);
+                    }
 
-                        int graphIndicesCount = _toBeSelectedGraphIndexesCache.Value.Count;
-                        if (graphIndicesCount > 0)
-                        {
-                            int firstGraphIndex = _toBeSelectedGraphIndexesCache.Value[0];
-                            EnsureRowVisible(firstGraphIndex);
-                        }
+                    // Scroll to first selected only if selection is not changed
+                    if (firstGraphIndex >= 0 && Rows[firstGraphIndex].Selected)
+                    {
+                        EnsureRowVisible(firstGraphIndex);
                     }
 
                     MarkAsDataLoadingComplete();
@@ -474,8 +483,6 @@ namespace GitUI.UserControls.RevisionGrid
             }
 
             return;
-
-            int ScrollTo() => _toBeSelectedGraphIndexesCache.Value.Count == 0 ? 0 : _toBeSelectedGraphIndexesCache.Value.Max();
 
             void MarkAsDataLoadingComplete()
             {
@@ -602,7 +609,7 @@ namespace GitUI.UserControls.RevisionGrid
                 }
             }
 
-            // The rows to be selected have just been selected. Prevent from selecting them again.
+            // The to-be-selected are handled. Prevent from selecting them again.
             ClearToBeSelected();
         }
 

GitUI/UserControls/RevisionGrid/RevisionGridControl.cs
@@ -944,6 +944,7 @@ namespace GitUI
                     // If the current checkout (HEAD) is changed, don't get the currently selected rows,
                     // select the new current checkout instead.
                     if (newCurrentCheckout != CurrentCheckout
+                        && newCurrentCheckout is not null
                         && currentlySelectedObjectIds is not null
                         && currentlySelectedObjectIds.Count == 1
                         && currentlySelectedObjectIds[0] == CurrentCheckout)
@@ -2908,7 +2909,7 @@ namespace GitUI
             public int VisibleRevisionCount => _revisionGridControl._gridView.RowCount;
 
             public bool IsDataLoadComplete =>
-                !_revisionGridControl._gridView.IsDataLoadComplete;
+                _revisionGridControl._gridView.IsDataLoadComplete;
 
             public void ClearSelection()
             {